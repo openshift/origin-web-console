33f701f40afbb7c64d4b9931ef8e46f9317cfe66
benjaminapetersen <admin@benjaminapetersen.me>
2016-05-19 14:32:18 -0400

    Add preamble, change var name to OS_ROOT, minor tweaks

--- hack/run-all.sh
+++ hack/run-all.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# Run all the important scripts in a sensible order,
-# handy for switching branches, etc
-wd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$wd/clean-deps.sh &&
-$wd/install-deps.sh &&
-echo '[build-assets]' &&
-grunt build &&
-echo '[test-assets]' &&
-grunt test &&
-grunt test-integration
+# This script is for local development usage & will run the
+# entire clean/build/install/test flow in order.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+OS_ROOT=$(dirname "${BASH_SOURCE}")/..
+${OS_ROOT}/hack/clean-deps.sh
+${OS_ROOT}/hack/install-deps.sh
+echo '[build-assets]'
+grunt build
+echo '[test-assets]'
+# SKIP_TESTS flag
+#if !-o SKIP_TESTS; then
+  grunt test
+  grunt test-integration
+#fi
 # $wd/test-integration-headless.sh &&
 # $wd/verify-dist.sh
