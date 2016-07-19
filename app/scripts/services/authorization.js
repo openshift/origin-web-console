845966ddb125417b6b11de7b080a712e59b83b5b
jhadvig <jhadvig@redhat.com>
2016-07-19 13:21:43 +0200

    Permissive mode for the what-can-i-do authrization

--- app/scripts/services/authorization.js
+++ app/scripts/services/authorization.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module("openshiftConsole")
-  .factory("AuthorizationService", function($q, $cacheFactory, Logger, $interval, DataService){
+  .factory("AuthorizationService", function($q, $cacheFactory, Logger, $interval, APIService, DataService){
     
     var currentProject = null;
     var cachedRulesByProject = $cacheFactory('rulesCache', {
           number: 10
         });
+    // Permisive mode will cause no checks to be done for the user actions.
+    var permissiveMode = false;
 
     var REVIEW_RESOURCES = ["localresourceaccessreviews", "localsubjectaccessreviews", "resourceaccessreviews", "selfsubjectrulesreviews", "subjectaccessreviews"];
 
@@ -40,7 +42,7 @@ angular.module("openshiftConsole")
     };
 
     // Check if user can create/update any resource on the 'Add to project' so the button will be displayed.
-    var canAddToProject = function(rules) {
+    var canAddToProjectCheck = function(rules) {
       return _.some(rules, function(rule) {
         return _.some(rule.resources, function(resource) {
           return checkResource(resource) && !_.isEmpty(_.intersection(rule.verbs ,(["*", "create", "update"])));
@@ -52,24 +54,33 @@ angular.module("openshiftConsole")
       var deferred = $q.defer();
       currentProject = projectName;
       var projectRules = cachedRulesByProject.get(projectName);
+      var rulesResource = "selfsubjectrulesreviews";
       if (!projectRules || projectRules.forceRefresh) {
-        Logger.log("AuthorizationService, loading user rules for " + projectName + " project");
-        var object = {kind: "SelfSubjectRulesReview",
-                      apiVersion: "v1"
-                    };
-        DataService.create('selfsubjectrulesreviews', null, object, {namespace: projectName}).then(
-          function(data) {
-            var normalizedData = normalizeRules(data.status.rules);
-            var canUserAddToProject = canAddToProject(data.status.rules);
-            cachedRulesByProject.put(projectName, {rules: normalizedData,
-                                                        canAddToProject: canUserAddToProject,
-                                                        forceRefresh: false,
-                                                        cacheTimestamp: _.now()
-                                                      });
-            deferred.resolve();
-          }, function() {
-            deferred.reject();
-        });
+        // Check if APIserver contains 'selfsubjectrulesreviews' resource. If not switch to permissive mode.
+        if (APIService.apiInfo(rulesResource)) {
+          Logger.log("AuthorizationService, loading user rules for " + projectName + " project");
+          var object = {kind: "SelfSubjectRulesReview",
+                        apiVersion: "v1"
+                      };
+          DataService.create(rulesResource, null, object, {namespace: projectName}).then(
+            function(data) {
+              var normalizedData = normalizeRules(data.status.rules);
+              var canUserAddToProject = canAddToProjectCheck(data.status.rules);
+              cachedRulesByProject.put(projectName, {rules: normalizedData,
+                                                      canAddToProject: canUserAddToProject,
+                                                      forceRefresh: false,
+                                                      cacheTimestamp: _.now()
+                                                    });
+              deferred.resolve();
+            }, function() {
+              permissiveMode = true;
+              deferred.resolve();
+          });
+        } else {
+          Logger.log("AuthorizationService, resource 'selfsubjectrulesreviews' is not part of APIserver. Switching into permissive mode.");
+          permissiveMode = true;
+          deferred.resolve();
+        }
       } else {
         // Using cached data.
         Logger.log("AuthorizationService, using cached rules for " + projectName + " project");
@@ -88,7 +99,9 @@ angular.module("openshiftConsole")
     var canI = function(verb, resource, projectName) {
       projectName = projectName || currentProject;
       var rules = getRulesForProject(projectName);
-      if (rules) {
+      if (permissiveMode) {
+        return true;
+      } else if (rules) {
         if (rules[resource]) {
           return _.contains(rules[resource], verb) || _.contains(rules[resource], '*');
         } else if (rules['*']) {
@@ -102,7 +115,11 @@ angular.module("openshiftConsole")
     };
 
     var canIAddToProject = function(projectName) {
-      return !!_.get(cachedRulesByProject.get(projectName || currentProject), ['canAddToProject']);
+      if (permissiveMode) {
+        return true;
+      } else {
+        return !!_.get(cachedRulesByProject.get(projectName || currentProject), ['canAddToProject']);
+      }
     };
 
     return {
