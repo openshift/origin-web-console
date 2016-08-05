2cb1d37aea7ef3a829978d2e22182a2363ce0392
jhadvig <jhadvig@redhat.com>
2016-08-05 13:12:52 +0200

    Rewriting BC editor trigger section

---
+++ app/scripts/directives/editWebhookTriggers.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('openshiftConsole')
+  // Element directive edit BC webhook triggers
+  .directive('editWebhookTriggers', function(ApplicationGenerator) {
+    return {
+      restrict: 'E',
+      scope: {
+        type: "@",
+        typeInfo: "@",
+        triggers: "=",
+        bcName: "@",
+        projectName: "@",
+        form: "="
+      },
+      templateUrl: 'views/directives/edit-webhook-triggers.html',
+      controller: function($scope) {
+        $scope.addWebhookTrigger = function(type) {
+          var webhook = {
+            disabled: false,
+            data: {
+              type: type
+            }
+          };
+          webhook.data[(type === "GitHub") ? "github" : "generic"] = {
+            secret: ApplicationGenerator._generateSecret()
+          };
+          $scope.triggers.push(webhook);
+          $scope.form.$setDirty();
+        };
+      }
+    };
+  });
