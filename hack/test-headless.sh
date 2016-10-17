f45f7041186c5d637b8fe6f360abc888483f1f10
Jessica Forrester <jforrest@redhat.com>
2016-10-17 17:12:49 -0400

    Revert "Add a cleanup trap for the Xvfb process"
    This reverts commit 68dcff8f31cf0993c218991462a75a4f5eddb60e.

--- hack/test-headless.sh
+++ hack/test-headless.sh
@@ -4,19 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-function cleanup_xvfb() {
-	if [[ -n "${xvfb_process:-}" ]]; then
-		kill -SIGTERM "${xvfb_process}"
-	fi
-}
-
-trap cleanup_xvfb EXIT
-
 echo "[INFO] Starting virtual framebuffer for headless tests..."
 export DISPLAY=':10'
 export SCREEN='0'
 Xvfb "${DISPLAY}" -screen "${SCREEN}" 1024x768x24 -ac &
-xvfb_process="$!"
 
 # Debian versions of `xrandr` want `-display` whereas RPM
 # versions want `--display`, so we'll just use `-d` to
