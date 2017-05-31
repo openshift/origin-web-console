17e3d33dc288e0b30b5e7dee9c9d6ebac9ad79e9
Robb Hamilton <rhamilto@redhat.com>
2017-05-31 14:58:54 -0400

    [WIP] Removing HTMLService and linkify filter as they're now in common

--- app/scripts/filters/util.js
+++ app/scripts/filters/util.js
@@ -495,18 +495,6 @@ angular.module('openshiftConsole')
   .filter('encodeURIComponent', function() {
     return window.encodeURIComponent;
   })
-  // Usage: <span ng-bind-html="text | linkify : '_blank'"></span>
-  //
-  // Prefer this to the AngularJS `linky` filter since it only matches http and
-  // https URLs. We've had issues with incorretly matching email addresses.
-  //
-  // https://github.com/openshift/origin-web-console/issues/315
-  // See also HTMLService.linkify
-  .filter('linkify', function(HTMLService) {
-    return function(text, target, alreadyEscaped) {
-      return HTMLService.linkify(text, target, alreadyEscaped);
-    };
-  })
   .filter('enableTechPreviewFeature', function(Constants) {
     return function(feature) {
       return _.get(Constants, ['ENABLE_TECH_PREVIEW_FEATURE', feature], false);
