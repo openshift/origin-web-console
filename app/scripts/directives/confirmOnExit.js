7ee435a8185e4a287dc7a7011ed93d3b32fd461c
Samuel Padgett <spadgett@redhat.com>
2017-06-08 08:49:38 -0400

    Bug 1459834 - Don't stop navigation when a user has blocked confirm dialogs

--- app/scripts/directives/confirmOnExit.js
+++ app/scripts/directives/confirmOnExit.js
@@ -2,13 +2,20 @@
 
 // http://stackoverflow.com/questions/14852802/detect-unsaved-changes-and-alert-user-using-angularjs
 angular.module("openshiftConsole")
-  .directive("confirmOnExit", function() {
+  .directive("confirmOnExit", function(Logger) {
     return {
       scope: {
         dirty: '=',
         message: '='
       },
       link: function($scope) {
+        // If the feature is disabled or the user has asked the browser to
+        // block dialogs, don't try to prompt.
+        if (_.get(window, 'OPENSHIFT_CONSTANTS.DISABLE_CONFIRM_ON_EXIT') ||
+            _.get(window, 'OPENSHIFT_CONSTANTS.CONFIRM_DIALOG_BLOCKED')) {
+          return;
+        }
+
         var getMessage = function() {
           return $scope.message || "You have unsaved changes. Leave this page anyway?";
         };
@@ -27,7 +34,27 @@ angular.module("openshiftConsole")
             return;
           }
 
-          if(!confirm(getMessage())) {
+          var start = new Date().getTime();
+
+          // Use a native confirm dialog to block code execution since we're in
+          // the location change listener.
+          var okToExit = confirm(getMessage());
+          if (okToExit) {
+            return;
+          }
+
+          // Workaround "Prevent this page from creating additional dialogs"
+          //
+          // If the response took less than 50ms, assume the confirm dialog was
+          // blocked by the browser. There's no API to detect that the user
+          // has told the browser to block these dialogs, and the user can't
+          // navigate away otherwise.
+          var end = new Date().getTime();
+          if ((end - start) < 50) {
+            // Remember that the user is blocking dialogs. This is a per session settings.
+            _.set(window, 'OPENSHIFT_CONSTANTS.CONFIRM_DIALOG_BLOCKED', true);
+            Logger.warn("Confirm on exit prompt appears to have been blocked by the browser.");
+          } else {
             event.preventDefault();
           }
         });
