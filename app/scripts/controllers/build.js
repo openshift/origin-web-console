bed0feeac9e270f737af8f454b7de72854281d33
Samuel Padgett <spadgett@redhat.com>
2017-06-15 11:58:22 -0400

    Bug 1461346 - Filter events by UID
    Use the API object UID rather than kind and name to filter events. This
    prevents events for showing for a previous object with the same name.

--- app/scripts/controllers/build.js
+++ app/scripts/controllers/build.js
@@ -58,6 +58,8 @@ angular.module('openshiftConsole')
       title: $routeParams.build
     });
 
+    var buildPod;
+    var annotation = $filter('annotation');
     var watches = [];
 
     var setLogVars = function(build) {
@@ -72,45 +74,6 @@ angular.module('openshiftConsole')
       }
     };
 
-    var buildResolved = function(build, action) {
-      $scope.loaded = true;
-      $scope.build = build;
-      setLogVars(build);
-
-      var buildNumber = $filter("annotation")(build, "buildNumber");
-      if (buildNumber) {
-        $scope.breadcrumbs[2].title = "#" + buildNumber;
-      }
-      if (action === "DELETED") {
-        $scope.alerts["deleted"] = {
-          type: "warning",
-          message: "This build has been deleted."
-        };
-      }
-    };
-
-    var buildRejected = function(e) {
-      $scope.loaded = true;
-      $scope.alerts["load"] = {
-        type: "error",
-        message: "The build details could not be loaded.",
-        details: $filter('getErrorDetails')(e)
-      };
-    };
-
-    var buildConfigResolved = function(buildConfig, action) {
-      if (action === "DELETED") {
-        $scope.alerts["deleted"] = {
-          type: "warning",
-          message: "Build configuration " + $scope.buildConfigName + " has been deleted."
-        };
-        $scope.buildConfigDeleted = true;
-      }
-      $scope.buildConfig = buildConfig;
-      $scope.buildConfigPaused = BuildsService.isPaused($scope.buildConfig);
-      updateCanBuild();
-    };
-
 
     ProjectsService
       .get($routeParams.project)
@@ -122,6 +85,67 @@ angular.module('openshiftConsole')
         // context into the log-viewer directive.
         $scope.projectContext = context;
         $scope.logOptions = {};
+
+        var updateEventObjects = function() {
+          if (buildPod) {
+            $scope.eventObjects = [$scope.build, buildPod];
+          } else {
+            $scope.eventObjects = [$scope.build];
+          }
+        };
+
+        var buildResolved = function(build, action) {
+          $scope.loaded = true;
+          $scope.build = build;
+          setLogVars(build);
+          updateEventObjects();
+
+          var buildNumber = annotation(build, "buildNumber");
+          if (buildNumber) {
+            $scope.breadcrumbs[2].title = "#" + buildNumber;
+          }
+          if (action === "DELETED") {
+            $scope.alerts["deleted"] = {
+              type: "warning",
+              message: "This build has been deleted."
+            };
+          }
+
+          var buildPodName;
+          if (!buildPod) {
+            buildPodName = annotation(build, 'buildPod');
+            if (buildPodName) {
+              // Don't show an error if we can't get the build pod. Often it will have been deleted.
+              DataService.get("pods", buildPodName, context, { errorNotification: false }).then(function(response) {
+                buildPod = response;
+                updateEventObjects();
+              });
+            }
+          }
+        };
+
+        var buildRejected = function(e) {
+          $scope.loaded = true;
+          $scope.alerts["load"] = {
+            type: "error",
+            message: "The build details could not be loaded.",
+            details: $filter('getErrorDetails')(e)
+          };
+        };
+
+        var buildConfigResolved = function(buildConfig, action) {
+          if (action === "DELETED") {
+            $scope.alerts["deleted"] = {
+              type: "warning",
+              message: "Build configuration " + $scope.buildConfigName + " has been deleted."
+            };
+            $scope.buildConfigDeleted = true;
+          }
+          $scope.buildConfig = buildConfig;
+          $scope.buildConfigPaused = BuildsService.isPaused($scope.buildConfig);
+          updateCanBuild();
+        };
+
         DataService
           .get("builds", $routeParams.build, context, { errorNotification: false })
           .then(function(build) {
