8b151dc04003030e0d7355910b067a0a3135e0e6
Robb Hamilton <rhamilto@redhat.com>
2017-09-05 14:20:37 -0400

    Adding Container Platform branding and doc links for 3.10

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -572,7 +572,7 @@ i();
 }
 
 angular.isUndefined(window.OPENSHIFT_CONSTANTS) && (window.OPENSHIFT_CONSTANTS = {}), angular.extend(window.OPENSHIFT_CONSTANTS, {
-HELP_BASE_URL: "https://docs.openshift.org/latest/",
+HELP_BASE_URL: "https://docs.openshift.com/container-platform/3.10/",
 HELP: {
 cli: "cli_reference/index.html",
 get_started_cli: "cli_reference/get_started_cli.html",
@@ -620,7 +620,25 @@ storage: "architecture/additional_concepts/storage.html",
 default: "welcome/index.html"
 },
 CLI: {
-"Latest Release": "https://github.com/openshift/origin/releases/latest"
+"Latest Release": "https://access.redhat.com/downloads/content/290"
+},
+CATALOG_HELP_RESOURCES: {
+links: [ {
+title: "Documentation",
+help: ""
+}, {
+title: "Interactive Learning Portal",
+href: "https://learn.openshift.com"
+}, {
+title: "Container Development Kit",
+href: "https://developers.redhat.com/products/cdk/overview/"
+}, {
+title: "YouTube",
+href: "https://www.youtube.com/user/rhopenshift"
+}, {
+title: "Blog",
+href: "https://blog.openshift.com"
+} ]
 },
 DEFAULT_HPA_CPU_TARGET_PERCENT: null,
 DISABLE_OVERVIEW_METRICS: !1,
