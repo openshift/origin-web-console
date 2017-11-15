26b22a24f32a590cbbf36bf566ed7c0db6e821ea
root <fuluheng@gmail.com>
2017-11-15 11:05:30 +0800

    Update deploy.

---
+++ deploy/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+tag=v0.0.4
+
+# workdir deploy
+echo "Start Copying Files"
+rm -rf .tmp
+rm -rf .tls
+
+cp -rf ../tls .tls
+
+mkdir -p .tmp
+
+cp -rf ../dist/404.html .tmp/404.html
+cp -rf ../dist/index.html .tmp/index.html
+sed -i 's/<base href="\/">/<base href="\/console\/">/g' .tmp/index.html
+
+mkdir -p .tmp/console
+cp -rf ../dist/images .tmp/console/images
+cp -rf ../dist/languages .tmp/console/languages
+cp -rf ../dist/scripts .tmp/console/scripts
+cp -rf ../dist/styles .tmp/console/styles
+
+cp -rf ../extensions/extensions.js .tmp/console/scripts/extensions.js
+cp -rf ../extensions/extensions.css  .tmp/console/styles/extensions.css
+
+cp -rf config.js.temp .tmp/console/config.js.temp
+
+echo "Start Docker Build"
+docker build -t origin-dm-web:$tag -f Dockerfile .
+
---
+++ deploy/start.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# replace nginx config
+NGINX_CONF_TEMP=/etc/nginx/conf.d/default.conf.temp
+NGINX_CONF_FILE=/etc/nginx/conf.d/default.conf
+
+cp -f $NGINX_CONF_TEMP $NGINX_CONF_FILE
+
+echo PORT
+if [ $LISTEN_PORT ]
+then
+sed -i "s/{{PORT}}/$LISTEN_PORT/g" $NGINX_CONF_FILE
+else
+sed -i "s/{{PORT}}/9000/g" $NGINX_CONF_FILE
+fi
+
+# replace js config
+JS_CONF_TEMP=/usr/share/nginx/html/console/config.js.temp
+JS_CONF_FILE=/usr/share/nginx/html/console/config.js
+
+cp -f $JS_CONF_TEMP $JS_CONF_FILE
+
+echo MASTER_HOST
+if [ $MASTER_HOST ]
+then
+sed -i "s/{{MASTER_HOST}}/$MASTER_HOST/g" $JS_CONF_FILE
+else
+sed -i "s/{{MASTER_HOST}}//g" $JS_CONF_FILE
+fi
+
+echo CONSOLE_HOST 
+if [ $CONSOLE_HOST ]
+then
+sed -i "s/{{CONSOLE_HOST}}/$CONSOLE_HOST/g" $JS_CONF_FILE
+else
+sed -i "s/{{CONSOLE_HOST}}//g" $JS_CONF_FILE
+fi
+
+echo LOGGING_URL
+if [ $LOGGING_URL ]
+then
+sed -i "s/{{LOGGING_URL}}/$LOGGING_URL/g" $JS_CONF_FILE
+else
+sed -i "s/{{LOGGING_URL}}//g" $JS_CONF_FILE
+fi
+
+echo METRICS_URL
+if [ $METRICS_URL ]
+then
+sed -i "s/{{METRICS_URL}}/METRICS_URL/g" $JS_CONF_FILE
+else
+sed -i "s/{{METRICS_URL}}//g" $JS_CONF_FILE
+fi
+
+sh -c "nginx -g \"daemon off;\""
+
