134f10ee3505991c463cd675587dca6ad593183a
Jakub Hadvig <jhadvig@redhat.com>
2018-02-02 17:43:42 +0100

    Uploading file without exec. permissions

--- app/scripts/directives/oscFileInput.js
+++ app/scripts/directives/oscFileInput.js
@@ -127,6 +127,10 @@ angular.module('openshiftConsole')
             if (files.length > 0 ) {
               scope.file = _.head(files);
               addFile(scope.file);
+            } else {
+              scope.uploadError = true;
+              scope.cleanInputValues();
+              Logger.error("Could not read file", e);
             }
             removeDropZoneClasses();
             $('.drag-and-drop-zone').trigger('putDropZoneFront', false);
@@ -185,6 +189,8 @@ angular.module('openshiftConsole')
               }
               if (!reader.error) {
                 scope.uploadError = false;
+              } else {
+                scope.cleanInputValues();
               }
             });
           };
