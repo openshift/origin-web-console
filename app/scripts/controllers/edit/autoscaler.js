632b0157eec80eb771ee6bdbe60dc94614f9abb2
Samuel Padgett <spadgett@redhat.com>
2018-02-05 14:54:57 -0500

    Bug 1540916: Set correct group in HPA scale target
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1540916

--- app/scripts/controllers/edit/autoscaler.js
+++ app/scripts/controllers/edit/autoscaler.js
@@ -90,48 +90,6 @@ angular.module('openshiftConsole')
           return;
         }
 
-        var createHPA = function() {
-          $scope.disableInputs = true;
-          hideErrorNotifications();
-          var hpa = {
-            apiVersion: "autoscaling/v1",
-            kind: "HorizontalPodAutoscaler",
-            metadata: {
-              name: $scope.autoscaling.name,
-              labels: keyValueEditorUtils.mapEntries(keyValueEditorUtils.compactEntries($scope.labels))
-            },
-            spec: {
-              scaleTargetRef: {
-                kind: $routeParams.kind,
-                name: $routeParams.name,
-                apiVersion: "extensions/v1beta1",
-                subresource: "scale"
-              },
-              minReplicas: $scope.autoscaling.minReplicas,
-              maxReplicas: $scope.autoscaling.maxReplicas,
-              targetCPUUtilizationPercentage: $scope.autoscaling.targetCPU
-            }
-          };
-
-          DataService.create(horizontalPodAutoscalerVersion, null, hpa, context)
-            .then(function(hpa) { // Success
-              NotificationsService.addNotification({
-                type: 'success',
-                message: 'Horizontal pod autoscaler ' + hpa.metadata.name + ' successfully created.'
-              });
-
-              navigateBack();
-            }, function(result) { // Failure
-              $scope.disableInputs = false;
-              NotificationsService.addNotification({
-                id: 'edit-hpa-error',
-                type: 'error',
-                message: 'An error occurred creating the horizontal pod autoscaler.',
-                details: getErrorDetails(result)
-              });
-            });
-        };
-
         var updateHPA = function(hpa) {
           $scope.disableInputs = true;
 
@@ -177,6 +135,47 @@ angular.module('openshiftConsole')
         }
 
         DataService.get(resourceGroup, $routeParams.name, context).then(function(resource) {
+          var createHPA = function() {
+            $scope.disableInputs = true;
+            hideErrorNotifications();
+            var hpa = {
+              apiVersion: "autoscaling/v1",
+              kind: "HorizontalPodAutoscaler",
+              metadata: {
+                name: $scope.autoscaling.name,
+                labels: keyValueEditorUtils.mapEntries(keyValueEditorUtils.compactEntries($scope.labels))
+              },
+              spec: {
+                scaleTargetRef: {
+                  kind: resource.kind,
+                  name: resource.metadata.name,
+                  apiVersion: resource.apiVersion
+                },
+                minReplicas: $scope.autoscaling.minReplicas,
+                maxReplicas: $scope.autoscaling.maxReplicas,
+                targetCPUUtilizationPercentage: $scope.autoscaling.targetCPU
+              }
+            };
+
+            DataService.create(horizontalPodAutoscalerVersion, null, hpa, context)
+              .then(function(hpa) { // Success
+                NotificationsService.addNotification({
+                  type: 'success',
+                  message: 'Horizontal pod autoscaler ' + hpa.metadata.name + ' successfully created.'
+                });
+
+                navigateBack();
+              }, function(result) { // Failure
+                $scope.disableInputs = false;
+                NotificationsService.addNotification({
+                  id: 'edit-hpa-error',
+                  type: 'error',
+                  message: 'An error occurred creating the horizontal pod autoscaler.',
+                  details: getErrorDetails(result)
+                });
+              });
+          };
+
           $scope.labels = _.map(
                             _.get(resource, 'metadata.labels', {}),
                             function(val, key) {
