7e4db3a3889eb209ca6f2e1ff80c89959c93a16e
Jameel Briones <jbriones@redhat.com>
2018-05-21 12:20:43 +0100

    [mobile-client-overview] mobile client overview enhancements
    Mobile client verview should only show an overview of the client and the
    things associated with it. Changes includes:
    - Add an alert widget when there are mobile services available to be bound.
    - Visualize the bound and unbound service in a donut graph.
    - List the latests 5 builds relating to the mobile client.

--- app/scripts/directives/overview/mobileClientRow.js
+++ app/scripts/directives/overview/mobileClientRow.js
@@ -4,9 +4,11 @@
   angular.module('openshiftConsole').component('mobileClientRow', {
     controller: [
       '$filter',
+      '$location',
       '$routeParams',
       'APIService',
       'AuthorizationService',
+      'BuildsService',
       'DataService',
       'ListRowUtils',
       'Navigate',
@@ -21,31 +23,62 @@
     templateUrl: 'views/overview/_mobile-client-row.html'
   });
 
-  function MobileAppRow($filter, $routeParams, APIService, AuthorizationService, DataService, ListRowUtils, Navigate, ServiceInstancesService) {
+  function MobileAppRow($filter, $location, $routeParams, APIService, AuthorizationService, BuildsService, DataService, ListRowUtils, Navigate, ServiceInstancesService) {
     var row = this;
     var serviceInstancesVersion = APIService.getPreferredVersion('serviceinstances');
     var serviceClassesVersion = APIService.getPreferredVersion('clusterserviceclasses');
+    var serviceBindingsVersion = APIService.getPreferredVersion('servicebindings');
+    var buildsVersion = APIService.getPreferredVersion('builds');
     var isServiceInstanceReady = $filter('isServiceInstanceReady');
     var isMobileService = $filter('isMobileService');
+    var watches = [];
+    var boundServices = 'Bound Services';
+    var unboundServices = 'Unbound Services';
     row.installType = '';
+    row.config = {
+      'chartId': _.get(row, 'apiObject.metadata.name') + '-services-info',
+      'legend': {
+        'show': true,
+        'position': 'right'
+      },
+      colors: {}
+    };
+    row.config.colors[boundServices] = $.pfPaletteColors.blue;
+    row.config.colors[unboundServices] = $.pfPaletteColors.orange;
+    row.chartHeight = 80;
 
     _.extend(row, ListRowUtils.ui);
 
-    row.$onInit = function() {
-      row.context = {namespace: _.get(row, 'apiObject.metadata.namespace')};
-      DataService.list(serviceClassesVersion, row.context, function(serviceClasses) {
-        serviceClasses = serviceClasses.by('metadata.name');
-        DataService.watch(serviceInstancesVersion, row.context, function(serviceinstances) {
-          row.services = _.filter(serviceinstances.by('metadata.name'), function(serviceInstance){
-            var serviceClass = _.get(serviceClasses, ServiceInstancesService.getServiceClassNameForInstance(serviceInstance));
-            return isMobileService(serviceClass) && isServiceInstanceReady(serviceInstance);
-          });
-        }, { errorNotification: false });
-      });
+    row.navigateToMobileTab = function(tab) {
+      var resource = _.get(row, 'apiObject.metadata.name');
+      var kind = _.get(row, 'apiObject.kind');
+      var namespace = _.get(row, 'apiObject.metadata.namespace');
+      var opts = {
+        tab: tab
+      };
+      return Navigate.resourceURL(resource, kind, namespace, null, opts);
+    }
+
+    row.navigateToBuild = function(build) {
+      var resource = _.get(build, 'metadata.labels.buildconfig');
+      var namespace = _.get(build, 'metadata.namespace');
+      return Navigate.resourceURL(resource, BUILD_CONFIG_KIND, namespace);
+    }
+
+    row.updateServicesInfo = function() {
+      if (row.services) {
+        row.servicesNotBoundCount = row.services.length - row.bindings.length;
+
+        row.data = [
+          [boundServices, row.bindings.length],
+          [unboundServices, row.servicesNotBoundCount]
+        ];
+      }
     };
 
     row.$onChanges = function(changes) {
-      if (changes.apiObject) {
+      var apiObjectChanges = changes.apiObject && changes.apiObject.currentValue;
+      if (apiObjectChanges) {
         row.bundleDisplay = row.apiObject.spec.appIdentifier;
         row.clientType = row.apiObject.spec.clientType.toUpperCase();
         switch (row.apiObject.spec.clientType) {
@@ -60,8 +93,46 @@
             break;
         }
       }
-    };
 
+      if (apiObjectChanges && !row.context) {
+        row.context = {namespace: _.get(row, 'apiObject.metadata.namespace')};
+      }
+
+      if (apiObjectChanges && !row.serviceInstancesWatched) {
+        row.serviceInstancesWatched = true;
+        DataService.list(serviceClassesVersion, row.context, function(serviceClasses) {
+          serviceClasses = serviceClasses.by('metadata.name');
+          watches.push(DataService.watch(serviceInstancesVersion, row.context, function(serviceinstances) {
+            row.services = _.filter(serviceinstances.by('metadata.name'), function(serviceInstance){
+              var serviceClass = _.get(serviceClasses, ServiceInstancesService.getServiceClassNameForInstance(serviceInstance));
+              return isMobileService(serviceClass) && isServiceInstanceReady(serviceInstance);
+            });
+            row.updateServicesInfo();
+          }, { errorNotification: false }));
+        });
+      }
+
+      if (apiObjectChanges && !row.bindingsWatched) {
+        row.bindingsWatched = true;
+        watches.push(DataService.watch(serviceBindingsVersion, row.context, function(bindingsData) {
+          row.bindings = _.filter(bindingsData.by('metadata.name'), function(binding) {
+            return _.get(binding.metadata.annotations, 'binding.aerogear.org/consumer') === _.get(row, 'apiObject.metadata.name');
+          });
+          row.updateServicesInfo();
+        }));
+      }
+
+      if (apiObjectChanges && !row.buildsWatched) {
+        row.buildsWatched = true;
+        watches.push(DataService.watch(buildsVersion, row.context, function(buildsData) {    
+          // Filter builds by mobile client id
+          var builds = _.filter(buildsData.by('metadata.name'), function(build) {
+            return _.get(build, 'metadata.labels.mobile-client-id') === _.get(row, 'apiObject.metadata.name');
+          });
+          row.builds = BuildsService.sortBuilds(builds, true);
+        }));
+      }
+    };
 
     row.mobileclientVersion = {
       group: "mobile.k8s.io",
@@ -81,5 +152,9 @@
     row.browseCatalog = function () {
       Navigate.toProjectCatalog(row.projectName, {category: 'mobile', subcategory: 'services'});
     };
+
+    row.$onDestroy = function() {
+      DataService.unwatchAll(watches);
+    }
   }
 })();
