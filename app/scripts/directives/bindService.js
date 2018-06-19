3a72efe42679be6405d6826ce6561c2dce644154
John Roche <jroche@redhat.com>
2018-06-19 20:24:32 +0100

    [MAR] Separate bound and unbound

--- app/scripts/directives/bindService.js
+++ app/scripts/directives/bindService.js
@@ -19,6 +19,7 @@
       target: '<',
       project: '<',
       onClose: '<',
+      onBindCreated: '<',
       parameterData: '<'
     },
     templateUrl: 'views/directives/bind-service.html'
@@ -46,6 +47,8 @@
     var serviceInstancesVersion = APIService.getPreferredVersion('serviceinstances');
     var serviceClassesVersion = APIService.getPreferredVersion('clusterserviceclasses');
     var servicePlansVersion = APIService.getPreferredVersion('clusterserviceplans');
+    var mobileBindingProviderAnnotation = $filter('annotationName')('mobileBindingProviderId');
+    var mobileBindingConsumerAnnotation = $filter('annotationName')('mobileBindingConsumerId');
 
     var preselectService = function(){
       var newestReady;
@@ -197,15 +200,14 @@
       });
     };
 
-    var getBindingMetadata = function(svcToBind, serviceClass) {
+    var getMobileBindingMetadata = function(svcToBind, serviceClass) {
       var consumerId = _.get(ctrl, 'parameterData.CLIENT_ID');
-
+      var annotations = {};
+      annotations[mobileBindingConsumerAnnotation] = consumerId;
+      annotations[mobileBindingProviderAnnotation] = _.get(svcToBind, 'metadata.name')
       return {
         generateName: consumerId.toLowerCase() + '-' + _.get(serviceClass, 'spec.externalMetadata.serviceName').toLowerCase() + '-',
-        annotations: {
-          'binding.aerogear.org/consumer':  consumerId,
-          'binding.aerogear.org/provider': _.get(svcToBind, 'metadata.name')
-        }
+        annotations: annotations
       };      
     };
 
@@ -259,24 +261,15 @@
       }
     };
 
-    ctrl.bindService = function() {
-      var svcToBind = ctrl.target.kind === 'ServiceInstance' ? ctrl.target : ctrl.serviceToBind;
-      var application = ctrl.bindType === 'application' ? ctrl.appToBind : undefined;
-
+    var createBinding = _.bind(function(svcToBind, application, serviceClass, parameterData, bindingMeta) {
       var context = {
         namespace: _.get(svcToBind, 'metadata.namespace')
       };
-
-      var serviceClass = BindingService.getServiceClassForInstance(svcToBind, ctrl.serviceClasses);
-
-      var bindingMeta = {};
-      if (ctrl.isMobileEnabled && isMobileService(serviceClass)) {
-        bindingMeta = getBindingMetadata(svcToBind, serviceClass);
-      }
-
-      BindingService.bindService(svcToBind, application, serviceClass, ctrl.parameterData, bindingMeta).then(function(binding){
+      BindingService.bindService(svcToBind, application, serviceClass, parameterData, bindingMeta).then(function(binding){
         ctrl.binding = binding;
         ctrl.error = null;
+
+        ctrl.bindCreated(ctrl.binding);
 
         bindingWatch = DataService.watchObject(BindingService.bindingResource, _.get(ctrl.binding, 'metadata.name'), context, function(binding) {
           ctrl.binding = binding;
@@ -284,11 +277,37 @@
       }, function(e) {
         ctrl.error = e;
       });
+    }, ctrl);
+
+    var bindMobileService = _.bind(function(svcToBind, serviceClass) {
+      var bindingMeta = getMobileBindingMetadata(svcToBind, serviceClass);
+      createBinding(svcToBind, undefined, serviceClass, ctrl.parameterData, bindingMeta);
+    }, ctrl);
+
+    var bindService = _.bind(function(svcToBind, serviceClass) {
+      var application = ctrl.bindType === 'application' ? ctrl.appToBind : undefined;
+      createBinding(svcToBind, application, serviceClass, ctrl.parameterData);
+    }, ctrl);
+
+    ctrl.bindService = function() {
+      var svcToBind = ctrl.target.kind === 'ServiceInstance' ? ctrl.target : ctrl.serviceToBind;
+      var serviceClass = BindingService.getServiceClassForInstance(svcToBind, ctrl.serviceClasses);
+      if (ctrl.isMobileEnabled && isMobileService(serviceClass)) {
+        bindMobileService(svcToBind, serviceClass);  
+      } else {
+        bindService(svcToBind, serviceClass);
+      }
     };
 
     ctrl.closeWizard = function() {
       if (_.isFunction(ctrl.onClose)) {
         ctrl.onClose();
+      }
+    };
+
+    ctrl.bindCreated = function(binding) {
+      if (_.isFunction(ctrl.onBindCreated)) {
+        ctrl.onBindCreated(binding);
       }
     };
   }
--- app/scripts/directives/mobileServiceInstanceRow.js
+++ app/scripts/directives/mobileServiceInstanceRow.js
@@ -32,76 +32,105 @@
                               ListRowUtils) {
     var row = this;
     var serviceInstanceDisplayName = $filter('serviceInstanceDisplayName');
+    var bindingProviderAnnotation = $filter('annotationName')('mobileBindingProviderId');
+    var bindingConsumerAnnotation = $filter('annotationName')('mobileBindingConsumerId');
     row.serviceBindingsVersion = APIService.getPreferredVersion('servicebindings');
+    var mobileClientsVersion = APIService.getPreferredVersion('mobileclients');
+    var servicePlansVersion = APIService.getPreferredVersion('clusterserviceplans');
+    var serviceBindingsVersion = APIService.getPreferredVersion('servicebindings');
     row.state = row.state || {};
-    var annotationSpace = "org.aerogear.binding." + this.apiObject.metadata.name;
-    var extendedAnnotationSpace = "org.aerogear.binding-ext." + this.apiObject.metadata.name;
+    var annotationSpace = 'org.aerogear.binding.' + this.apiObject.metadata.name;
+    var extendedAnnotationSpace = 'org.aerogear.binding-ext.' + this.apiObject.metadata.name;
     var watches = [];
-    var bindingsWatch = false;
-    row.bindingsLimit = _.get(row.serviceClass, "spec.externalMetadata.bindingsLimit", 1);
+    row.bindingInProgress = false;
+    var isBindingReady = $filter('isBindingReady');
+    row.bindingsLimit = _.get(row.serviceClass, 'spec.externalMetadata.bindingsLimit', 1);
     _.extend(row, ListRowUtils.ui);
 
-    $scope.$on('$destroy', function(){
-        DataService.unwatchAll(watches);
-        DataService.unwatch(bindingsWatch);
-    });
 
-    watches.push(DataService.watchObject(APIService.getPreferredVersion('mobileclients'), $routeParams.mobileclient, {namespace: $routeParams.project}, function(mobileClient) {
-        row.mobileClient = mobileClient;
+    row.$onChanges = function(changes) {
+      row.displayName = serviceInstanceDisplayName(row.apiObject, row.serviceClass);
 
-        row.annotations = _.filter(row.mobileClient.metadata.annotations, function(annotation, name){
-          return name.split("/")[0] === annotationSpace;
+      if (changes.apiObject && changes.apiObject.isFirstChange()) {
+        row.getClient()
+          .then(function(client) {
+            row.mobileClient = client;
+            row.serviceType = _.get(row, 'serviceClass.spec.externalMetadata.serviceName');
+
+            row.sortAnnotations();
+            row.getBindingParametersData();
+            row.watchServiceBindings();
+            row.watchClient();
+          });       
+      }
+    };
+
+    row.getClient = function() {
+      return DataService.list(mobileClientsVersion, {namespace: $routeParams.project})
+        .then(function(mobileClients) {
+          return _.find(mobileClients.by('metadata.name'), function(client) {
+            return client.metadata.name === $routeParams.mobileclient;
+          });
+      });
+    };
+
+    row.sortAnnotations = function() {
+      var annotations = _.get(row, 'mobileClient.metadata.annotations', []);
+      row.annotations = _(annotations)
+          .filter(function(annotation, name) {
+          return name.split('/')[0] === annotationSpace;
         })
         .map(JSON.parse)
-        .sort(function(a, b) {
-          if (a.label < b.label) {
-            return -1;
-          }
-          if (a.label > b.label) {
-            return 1;
-          }
-          return 0;
+        .sortBy(function(annotation) {
+          return annotation.label;
+        })
+        .value();
+
+      row.extendedAnnotations = _(annotations)
+        .pickBy(function(annotation, name) {
+          return name.split('/')[0] === extendedAnnotationSpace;
+        })
+        .transform(function(result, strVal, name) {
+          var key = name.split('/')[1];
+          result[key] = JSON.parse(strVal);
+        })
+        .value();
+    };
+
+    row.getBindingParametersData = function() {
+      DataService.list(servicePlansVersion, {}, function(servicePlans) {
+        var servicePlanData = servicePlans.by('metadata.name');
+        row.providerServicePlan = _.find(servicePlanData, function(plan) {
+          return row.serviceClass.metadata.name === plan.spec.clusterServiceClassRef.name;
+        });
+        var kind = row.mobileClient.kind.toLowerCase();
+        var bindDataPath = 'providerServicePlan.spec.externalMetadata.' + kind + '_bind_parameters_data';
+        var bindData = _.get(row, bindDataPath, []);
+        row.bindData = _(bindData).map(JSON.parse).value();
+      });
+    };
+
+    row.watchServiceBindings = function() {
+      watches.push(DataService.watch(serviceBindingsVersion, {namespace: $routeParams.project}, function(bindings) {
+        var clientBindings = _.filter(bindings.by('metadata.name'), function(binding) {
+          return _.get(binding.metadata.annotations, bindingConsumerAnnotation) === row.mobileClient.metadata.name &&
+                _.get(binding.metadata.annotations, bindingProviderAnnotation) === row.apiObject.metadata.name;
         });
 
-        row.serviceType = _.get(row, "serviceClass.spec.externalMetadata.serviceName"); // safe get
-
-        row.extendedAnnotations = _(row.mobileClient.metadata.annotations)
-          .pickBy(function(annotation, name){
-            return name.split("/")[0] === extendedAnnotationSpace;
-          })
-          .transform(function(result, strVal, name) {
-            var key = name.split("/")[1];
-            result[key] = JSON.parse(strVal);
-          })
-          .value();
-
-        row.servicePlanPromise = DataService.list(APIService.getPreferredVersion('clusterserviceplans'), {}, function(servicePlans) {
-          var servicePlanData = servicePlans.by('metadata.name');
-          row.providerServicePlan = _.find(servicePlanData, function(plan) {
-            return row.serviceClass.metadata.name === plan.spec.clusterServiceClassRef.name;
-          });
-          var kind = row.mobileClient.kind.toLowerCase();
-          var bindDataPath = 'providerServicePlan.spec.externalMetadata.' + kind + '_bind_parameters_data';
-          var bindData = _.get(row, bindDataPath, []);
-          row.bindData = _(bindData).map(JSON.parse).value();
+        row.bindings = _.filter(clientBindings, function(binding) {
+          return isBindingReady(binding) || _.get(binding, 'status.currentOperation') === 'Unbind';
         });
+        row.bindingInProgress = _.some(clientBindings, function(binding) {
+          return !isBindingReady(binding);
+        });
+      }));
+    };
 
-        if(!bindingsWatch){
-          bindingsWatch = DataService.watch(APIService.getPreferredVersion("servicebindings"), {namespace: $routeParams.project}, function(bindings){
-            row.bindings = _.filter(bindings.by("metadata.name"), function(binding){
-              return _.get(binding.metadata.annotations, "binding.aerogear.org/consumer") === row.mobileClient.metadata.name &&
-                    _.get(binding.metadata.annotations, "binding.aerogear.org/provider") === row.apiObject.metadata.name;
-            });
-
-            row.bindingInProgress = _.sumBy(row.bindings, function(binding){
-              return $filter('isBindingReady')(binding) ? 0 : 1;
-            });
-          });
-        }
-    }));
-
-    row.$onChanges = function() {
-      row.displayName = serviceInstanceDisplayName(row.apiObject, row.serviceClass);
+    row.watchClient = function() {
+      watches.push(DataService.watchObject(mobileClientsVersion, $routeParams.mobileclient, {namespace: $routeParams.project}, function(mobileClient) {
+        row.mobileClient = mobileClient;
+        row.sortAnnotations();
+      }));
     };
 
     row.closeOverlayPanel = function() {
@@ -109,11 +138,11 @@
     };
 
     row.showOverlayPanel = function(panelName, state) {
-      row.parameterData = _.reduce(row.bindData, function(acc, current) {
-        if (current.type === 'path') {
-          acc[current.name] = _.get(row, 'mobileClient.' + current.value);
-        } else if (current.type === 'default') {
-          acc[current.name] = current.value;
+      row.parameterData = _.reduce(row.bindData, function(acc, parameterData) {
+        if (parameterData.type === 'path') {
+          acc[parameterData.name] = _.get(row, 'mobileClient.' + parameterData.value);
+        } else if (parameterData.type === 'default') {
+          acc[parameterData.name] = parameterData.value;
         }
         return acc;
       },{});
@@ -121,5 +150,14 @@
       _.set(row, 'overlay.panelName', panelName);
       _.set(row, 'overlay.state', state);
     };
+
+    row.onBindCreated = function() {
+      var uid = _.get(row.apiObject, 'metadata.uid', '');
+      sessionStorage.setItem('overview/expand/' + uid, 'true');
+    };
+
+    $scope.$on('$destroy', function(){
+      DataService.unwatchAll(watches);
+    });
   }
 })();
