f1d719f2d7b34bc50f6dd41f7b9a09959b26b2ec
John Roche <jroche@redhat.com>
2018-06-29 09:31:58 +0100

    [mobile-client-view] Only show 1 sdk info per service not per binding

--- app/scripts/directives/mobileServiceSdkDocs.js
+++ app/scripts/directives/mobileServiceSdkDocs.js
@@ -34,31 +34,37 @@
 
       if (clientChanges && serviceClassChanges && watches.length === 0) {
         watches.push(DataService.watch(serviceBindingsVersion, {namespace: ctrl.context}, function(bindingsData) {
-          var mobileClientID = _.get(ctrl, 'mobileClient.metadata.name')
+          var mobileClientID = _.get(ctrl, 'mobileClient.metadata.name');
           ctrl.bindings = _.filter(bindingsData.by('metadata.name'), function(binding) {
             return _.get(binding, ['metadata', 'annotations', mobileBindingConsumerID]) === mobileClientID;
           });
-          ctrl.addDetailsToBindings();
+          ctrl.createServiceSDKInfo();
         }));
       }
     };
 
-    ctrl.addDetailsToBindings = function () {
+    ctrl.createServiceSDKInfo = function () {
       DataService.list(serviceInstanceVersion, {namespace: ctrl.context}, function (serviceInstances) {
         var serviceInstancesData = serviceInstances.by('metadata.name');
-        ctrl.serviceSdkInfo = _.map(ctrl.bindings, function(binding) {
-          var bindingProviderInstance = serviceInstancesData[_.get(binding, ['metadata', 'annotations', mobileBindingProviderID], "")];
-          var serviceClass = ctrl.serviceClasses[_.get(bindingProviderInstance, 'spec.clusterServiceClassRef.name')];
+        ctrl.serviceSdkInfo = _(ctrl.bindings)
+          .uniqBy(function(binding) {
+            var bindingProviderInstance = serviceInstancesData[_.get(binding, ['metadata', 'annotations', mobileBindingProviderID], "")];
+            return _.get(bindingProviderInstance, 'metadata.name');
+          })
+          .map(function(binding) {
+            var bindingProviderInstance = serviceInstancesData[_.get(binding, ['metadata', 'annotations', mobileBindingProviderID], "")];
+            var serviceClass = ctrl.serviceClasses[_.get(bindingProviderInstance, 'spec.clusterServiceClassRef.name')];
 
-          return {
-            displayName: _.get(serviceClass, 'spec.externalMetadata.displayName'),
-            serviceInstanceName: _.get(bindingProviderInstance, 'metadata.name'),
-            description: _.get(serviceClass, 'spec.description'),
-            sdkDocs: _.get(serviceClass, 'spec.externalMetadata.sdk-docs-' + ctrl.mobileClient.spec.clientType),
-            logo: _.get(serviceClass, 'spec.externalMetadata.imageUrl'),
-            iconClass: _.get(serviceClass, ['spec', 'externalMetadata', 'console.openshift.io/iconClass'])
-          };
-        });
+            return {
+              displayName: _.get(serviceClass, 'spec.externalMetadata.displayName'),
+              serviceInstanceName: _.get(bindingProviderInstance, 'metadata.name'),
+              description: _.get(serviceClass, 'spec.description'),
+              sdkDocs: _.get(serviceClass, 'spec.externalMetadata.sdk-docs-' + ctrl.mobileClient.spec.clientType),
+              logo: _.get(serviceClass, 'spec.externalMetadata.imageUrl'),
+              iconClass: _.get(serviceClass, ['spec', 'externalMetadata', 'console.openshift.io/iconClass'])
+            };
+          })
+          .value();
       });
     };
 
